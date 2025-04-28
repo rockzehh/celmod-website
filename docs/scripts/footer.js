ac9bc3bbaef5929c4b238ebbb4c8f01c750aac8f
King Nothing <zacharyh1199@gmail.com>
2025-04-28 01:04:35 -0500

    Fixed scripts

--- docs/scripts/footer.js
+++
@@ -1,4 +0,0 @@
-var footer = document.createElement('div');
-footer.setAttribute('id', 'footer');
-footer.setAttribute('style', 'text-align: center; margin-left: auto; margin-right: auto; background-image: url(https://raw.githubusercontent.com/rockzehh/celmod/refs/heads/main/docs/images/cmtextbg_bottom.png); height: 25px; width: 677px;');
-document.body.appendChild(footer);
